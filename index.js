a5f06ff85d5905e886fc0503ed1a3f0e0d8c8830
Franco Aronne <francroaronne1990@gmail.com>
2018-04-27 13:49:20 -0300

    Api-xml v1.2 - puerto 8080

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-var fs = require('fs'),
-    xml2js = require('xml2js');
+var fs = require('fs');
 
 
 router.use(bodyParser.json());
